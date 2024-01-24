da8f0b79ac3c7cb01309ceba0f6525d4b0f75a53
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-01-24 16:49:28 +0700

    setting up

---
+++ src/main/java/com/example/basicjpa_sd18202/BasicJpaSd18202Application.java
@@ -0,0 +1,13 @@
+package com.example.basicjpa_sd18202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJpaSd18202Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicJpaSd18202Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicjpa_sd18202/BasicJpaSd18202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicjpa_sd18202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicJpaSd18202ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
